84b39bad4661c2ec96453c42122e29416de5757b
dkbac <dkbac159@gmail.com>
2021-07-06 17:09:57 +0700

    Implement Splash page part 1

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,24 @@
+//Packages
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  final VoidCallback onInitializationComplete;
+
+  const SplashPage({
+    required Key key,
+    required this.onInitializationComplete,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _SplashPageState();
+  }
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
